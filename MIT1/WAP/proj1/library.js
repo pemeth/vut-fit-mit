00b85bcff85d68b19c752bcb0139a83cd758b9e3
Patrik Nemeth <patriknemethpn@gmail.com>
2021-02-26 13:23:10 +0100

    Begin prototyping the employee classes
    There is a base Employee class, which all other specific employee
    types inherit.

--- MIT1/WAP/proj1/library.js
+++ MIT1/WAP/proj1/library.js
@@ -3,3 +3,58 @@
  *	individual employees
  */
 
+class Employee {
+	#name;
+	#job;
+	#nextInChain;
+
+	constructor(name,job) {
+		this.#name = name;
+		this.#job = job;
+	}
+
+	setNext(next) {
+		this.#nextInChain = next;
+	}
+}
+
+/**
+ * TODO: idea for a pool of employees of same rank, which could pass tasks 
+ * to each other based on whether or not they were busy (or something else)
+ */
+class EmployeePool {
+	#pool = [];
+
+	pushEmp(emp) {
+		if (emp instanceof Employee) {
+			this.#pool.push(emp);
+		} else {
+			throw TypeError('Expected type Employee');
+		}
+	}
+}
+
+class Ceo extends Employee {
+	constructor(name, job) {
+		super(name, job);
+	}
+}
+
+class Bureaucrat extends Employee {
+	constructor(name, job) {
+		super(name, job);
+	}
+}
+
+class Caretaker extends Employee {
+	constructor(name, job) {
+		super(name, job);
+	}
+}
+
+class Feeder extends Employee {
+	constructor(name, job) {
+		super(name, job);
+	}
+}
+
