98212e1e887cc457b0190e4576da2c6d1b59b3df
Patrik Nemeth <patriknemethpn@gmail.com>
2021-03-22 20:19:48 +0100

    Parse input ciphertext into a vector
    The input is converted into uppercase and only uppercase letters are
    pushed to the vector for the ciphertext, as the task said to ignore
    everything other than letters.

--- MIT1/KRY/proj1/main.cpp
+++ MIT1/KRY/proj1/main.cpp
@@ -4,8 +4,25 @@
  * @author Patrik Nemeth xnemet04
  */
 #include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <tuple>
 
 int main(int argc, char *argv[]) {
+	if (argc != 1) {
+		return EXIT_FAILURE;
+	}
+
+	// get input ciphertext
+	std::vector<char> ctext;
+	int c;
+	while ((c = getchar()) != EOF) {
+		c = toupper(c);
+		if (c >= 65 && c <= 90) {
+			ctext.push_back(c);
+		}
+	}
 
 	return 0;
 }
