93e87ecb0e82ebe2e95868e58db8938d9b2c30fc
Patrik Nemeth <patriknemethpn@gmail.com>
2021-03-24 14:00:29 +0100

    Add quicksort for the vector of (factor,cnt) tuples
    So that the factors with the highest counts are at the beginning of the
    vector.

--- MIT1/KRY/proj1/main.cpp
+++ MIT1/KRY/proj1/main.cpp
@@ -19,6 +19,12 @@ std::vector<std::tuple<ulong_t, ulong_t> >
 	getFactorCounts(std::vector<ulong_t> distances);
 int
 	isFactorInVector(ulong_t factor, std::vector<std::tuple<ulong_t, ulong_t> > vect);
+void swap
+	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int idx1, int idx2);
+int partition
+	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
+void quickSortTuplesBySnd
+	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
 
 int main(int argc, char *argv[]) {
 	if (argc != 1) {
@@ -38,6 +44,8 @@ int main(int argc, char *argv[]) {
 	std::vector<ulong_t> distances = getDistances(&ctext);
 	std::vector<std::tuple<ulong_t, ulong_t> > factorTuples =
 		getFactorCounts(distances);
+
+	quickSortTuplesBySnd(&factorTuples, 0, factorTuples.size() - 1);
 
 	return 0;
 }
@@ -142,4 +150,67 @@ std::vector<ulong_t> getDistances(std::vector<char> *ctext)
 	}
 
 	return distances;
+}
+
+/**
+ * Quick sort vector of tuples by the second (snd) tuple value.
+ *
+ * @param arr is a pointer to the vector to be sorted.
+ * @param left the leftmost starting index - on first call must be 0.
+ * @param right the rightmost starting index - on first call must be
+ * last index of `vect`.
+ */
+void quickSortTuplesBySnd
+(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right)
+{
+	if (left < right) {
+		ulong_t p = partition(vect, left, right);
+
+		quickSortTuplesBySnd(vect, left, p - 1);
+		quickSortTuplesBySnd(vect, p + 1, right);
+	}
+}
+
+/**
+ * Partitioning function for quicksort.
+ *
+ * @param vect a pointer to the vector to be sorted.
+ * @param left the left index.
+ * @param right the right index.
+ * @returns the index at which the next iteration of quicksort should part the
+ * sorted vector.
+ */
+int partition
+(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right)
+{
+	ulong_t pivot = std::get<1>((*vect)[right]); // pivot = 3
+
+	ulong_t i = left - 1;
+
+	for (int j = left; j <= right - 1; j++) {
+		if (std::get<1>((*vect)[j]) > pivot) {
+			i++;
+			swap(vect, i, j);
+		}
+	}
+
+	i++;
+	swap(vect, i, right);
+	return (i);
+}
+
+/**
+ * Swaps two elements of `vect` at indices `idx1` and `idx2`.
+ * No out of bounds checking is done for the indices.
+ *
+ * @param vect a pointer to the vector, in whcihc the swap will take place.
+ * @param idx1 is the index of one of the elements to be swapped.
+ * @param idx2 is the index of the other element to be swapped.
+ */
+void swap
+(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int idx1, int idx2)
+{
+	std::tuple<ulong_t, ulong_t> tmp = (*vect)[idx1];
+	(*vect)[idx1] = (*vect)[idx2];
+	(*vect)[idx2] = tmp;
 }
