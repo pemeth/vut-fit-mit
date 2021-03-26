5dca44d761a35cff1507a06adf8d4b9e944cc6b5
Patrik Nemeth <patriknemethpn@gmail.com>
2021-03-26 19:00:49 +0100

    Unify formatting
    The function declarations have a unified format.

--- MIT1/KRY/proj1/kry.hpp
+++ MIT1/KRY/proj1/kry.hpp
@@ -12,14 +12,15 @@
 
 typedef unsigned long int ulong_t;
 
-double calcMg
-    (const std::array<double, 26> *p, std::array<ulong_t, 26> *f, ulong_t n, int g);
-std::array<double, 26> getFreqs
-    (std::array<ulong_t, 26> *letterCounts, ulong_t colLength);
-int guessShift
-    (const std::array<double, 26> *realFreqs, std::array<ulong_t, 26> *ctextFreqs, ulong_t colLength);
-std::vector<char> findKey
-    (std::vector<char> *ctext, ulong_t keylength);
+// cracker.cpp
+double
+	calcMg(const std::array<double, 26> *p, std::array<ulong_t, 26> *f, ulong_t n, int g);
+int
+	guessShift(const std::array<double, 26> *realFreqs, std::array<ulong_t, 26> *ctextFreqs, ulong_t colLength);
+std::vector<char>
+	findKey(std::vector<char> *ctext, ulong_t keylength);
+
+// kasiski.cpp
 std::vector<ulong_t>
 	getDistances(std::vector<char> *ctext);
 std::vector<ulong_t>
@@ -27,22 +28,26 @@ std::vector<ulong_t>
 std::vector<std::tuple<ulong_t, ulong_t> >
 	getFactorCounts(std::vector<ulong_t> distances);
 int
-	isFactorInVector(ulong_t factor, std::vector<std::tuple<ulong_t, ulong_t> > vect);
-void swap
-	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int idx1, int idx2);
-int partition
-	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
-void quickSortTuplesBySnd
-	(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
+	isFactorInVector (ulong_t factor, std::vector<std::tuple<ulong_t, ulong_t> > vect);
 ulong_t
 	getLastRisingKeylength(std::vector<std::tuple<ulong_t, ulong_t> > *factors);
-std::array<ulong_t, 26>
-    getLetterCounts(std::vector<char> *ctext);
+
+// friedman.cpp
 double
-    indexOfCoincidence(std::array<ulong_t, 26> letterCounts, ulong_t textLength);
-double getKeylengthFriedman
-    (std::array<ulong_t, 26> letterCounts, ulong_t textLength);
+	indexOfCoincidence(std::array<ulong_t, 26> letterCounts, ulong_t textLength);
+double
+	getKeylengthFriedman(std::array<ulong_t, 26> letterCounts, ulong_t textLength);
 std::array<ulong_t, 26>
-    getLetterCounts(std::vector<char> *text, ulong_t beginAt, ulong_t skip);
+	getLetterCounts(std::vector<char> *ctext);
+std::array<ulong_t, 26>
+	getLetterCounts(std::vector<char> *text, ulong_t beginAt, ulong_t skip);
+
+// lib.cpp
+void
+	swap(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int idx1, int idx2);
+int
+	partition(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
+void
+	quickSortTuplesBySnd(std::vector<std::tuple<ulong_t, ulong_t> > *vect, int left, int right);
 
 #endif /* KRY_HPP */
