8cebe46854e3f0f8af118298257e73d1e2a072ae
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-02 16:45:54 +0200

    Add option to specify testing input directory
    If an argument is given, then it is taken as the directory, in which the
    testing *.ctxt ciphertext files are located. Otherwise the *.ctxt files
    are taken from the current direcory.

--- MIT1/KRY/proj1/test.sh
+++ MIT1/KRY/proj1/test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
+# If no argument is given, then the testing files are in current directory
+#	otherwise they are in the directory passed as 1st argument
 dir=`pwd`
+if [ ! -z "$1" ]; then
+	dir=`pwd`"/$1"
+fi
+
 for file in "$dir"/*.ctxt
 do
 	printf '%s\n' "$file"
