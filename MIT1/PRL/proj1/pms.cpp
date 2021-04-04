9337f6188bf153d527548710372e29a10da55d3a
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-04 20:15:22 +0200

    Add input loading
    Load the 16 values into a static array and print them out.

--- MIT1/PRL/proj1/pms.cpp
+++ MIT1/PRL/proj1/pms.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-
 #include <mpi.h>
+
+#define VALUES_CNT 16
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
+    int rank;
+    int values[VALUES_CNT];
 
-    std::cout << "HELLO\n";
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int c, i=0;
+    if (rank == 0) {
+        // Load 16 unsorted values from stdin
+        while ((c = getc(stdin)) != EOF && i < VALUES_CNT) {
+            values[i] = (int) c;
+            std::cout << values[i] << ' ';
+        }
+        std::cout << '\n';
+    }
 
     MPI_Finalize();
     return 0;
