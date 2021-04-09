db13506a051e604f6525d87d8036967aa68c9035
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-09 18:20:06 +0200

    Replace if/else with guards
    More Haskell-ey this way.

--- MIT1/FLP/proj1/Simplifier.hs
+++ MIT1/FLP/proj1/Simplifier.hs
@@ -15,50 +15,47 @@ isInLRules nterm (rule : rest) =
 -- Returns an array of the right sides of rules for a specific non-terminal.
 getRRulesOfNterm :: Char -> [(Char, String)] -> [String]
 getRRulesOfNterm _ [] = []
-getRRulesOfNterm nterm (rule : rules) =
-    if (nterm == fst rule) then
-        (snd rule) : (getRRulesOfNterm nterm rules)
-    else
-        getRRulesOfNterm nterm rules
+getRRulesOfNterm nterm (rule : rules)
+    | nterm == fst rule = (snd rule) : (getRRulesOfNterm nterm rules)
+    | otherwise         = getRRulesOfNterm nterm rules
 
 -- Step (2) of algorithm 4.1 from TIN scripts
 buildNextNi :: String -> String -> String -> [(Char, String)] -> String
 buildNextNi _ [] _ _ = []
-buildNextNi ni (ntA : ntAs) terms rules =
-    if checkAlphas (getRRulesOfNterm ntA rules) (terms ++ ni) then
-        let currNi = ni ++ (ntA : []) in ntA : (buildNextNi currNi ntAs terms rules)
-    else
-        buildNextNi ni ntAs terms rules
+buildNextNi ni (ntA : ntAs) terms rules
+    | addToNi   =
+        let currNi = ni ++ (ntA : []) in
+            ntA : (buildNextNi currNi ntAs terms rules)
+    | otherwise = buildNextNi ni ntAs terms rules
+    where
+        addToNi = checkAlphas (getRRulesOfNterm ntA rules) (terms ++ ni)
 
 -- Builds the 'Nt' set according to algorithm 4.1 from TIN.
 makeSetNt :: String -> String -> String -> [(Char, String)] -> String
-makeSetNt ni nterm term rules =
-    if ni' /= ni then
-        nub (ni' ++ (makeSetNt ni' nterm term rules))
-    else
-        []
-    where ni' = buildNextNi ni nterm term rules
+makeSetNt ni nterm term rules
+    | ni' /= ni = nub (ni' ++ (makeSetNt ni' nterm term rules))
+    | otherwise = []
+    where
+        ni' = buildNextNi ni nterm term rules
 
 -- Builds an iteration of the 'V' (so technically builds Vi)
 -- set for algorithm 4.2.
 buildNextVi :: String -> [(Char,String)] -> String
 buildNextVi _ [] = []
-buildNextVi vi (rule : rules) =
-    if (fst rule) `elem` vi then
-        let currVi = nub (snd rule ++ vi) in currVi ++ (buildNextVi currVi rules)
-    else
-        buildNextVi vi rules
+buildNextVi vi (rule : rules)
+    | (fst rule) `elem` vi  =
+        let currVi = nub (snd rule ++ vi) in
+            currVi ++ (buildNextVi currVi rules)
+    | otherwise             = buildNextVi vi rules
 
 -- Builds the 'V' set based on algorithm 4.2 from TIN.
 -- Takes an initialization value of Vi (the start symbol of the CFG), which
 -- is an array of one non-terminal symbol. Returns the 'V' set.
 makeSetV :: String -> [(Char,String)] -> String
 makeSetV "" _ = error "Algorithm needs an initialization value"
-makeSetV vi rules =
-    if vi' /= vi then
-        nub (vi' ++ (makeSetV vi' rules))
-    else
-        vi
+makeSetV vi rules
+    | vi' /= vi = nub (vi' ++ (makeSetV vi' rules))
+    | otherwise = vi
     where
         -- Filter out empty strings (#)
         vi' = filter (\x -> isAlpha x) (buildNextVi vi rules)
