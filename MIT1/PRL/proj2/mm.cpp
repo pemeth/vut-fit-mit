d4a257aaac76167806263eb41da426373207d1d8
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-21 14:58:32 +0200

    Set up file

---
+++ MIT1/PRL/proj2/mm.cpp
@@ -0,0 +1,11 @@
+/**
+ * Second PRL project: matrix mesh multiplication using OpenMPI.
+ * @author Patrik Nemeth (xnemet04)
+ */
+
+#include <mpi.h>
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
