d0f100d0f0656a3a1510c0eddf79422cc71cfcdc
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-21 15:09:50 +0200

    Set up MPI

--- MIT1/PRL/proj2/mm.cpp
+++ MIT1/PRL/proj2/mm.cpp
@@ -2,10 +2,21 @@
  * Second PRL project: matrix mesh multiplication using OpenMPI.
  * @author Patrik Nemeth (xnemet04)
  */
-
+#include <iostream>
 #include <mpi.h>
 
-int main(int argc, char const *argv[])
+#define FILE_MAT1 "mat1"
+#define FILE_MAT2 "mat2"
+
+int main(int argc, char *argv[])
 {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    MPI_Finalize();
     return 0;
 }
