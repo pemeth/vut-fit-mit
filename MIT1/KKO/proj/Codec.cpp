0bba289f369d2a4f70e5366e11dc76595b2c56f0
Patrik Nemeth <patriknemethpn@gmail.com>
2021-04-29 12:25:49 +0200

    Add methods to figure out the best RLE direction
    So far only horizontal and veritcal directions are checked.

--- MIT1/KKO/proj/Codec.cpp
+++ MIT1/KKO/proj/Codec.cpp
@@ -8,3 +8,77 @@ Codec::Codec(Image *img)
 Codec::~Codec()
 {
 }
+
+/**
+ * Check which direction (horizontal or vertical) has the lowest number of
+ * same-value run length changes.
+ * I.e. binary image of width 4:
+ * 1100
+ * 0000
+ * has 3 changes in the vertical direction, but only 1 change horizontally.
+ * @returns The best encoding direction for RLE. The direction values are
+ * defined as DIRECTION_* macros in "Code.hpp".
+ */
+uint8_t Codec::best_encoding_direction()
+{
+    uint32_t chg_horiz = changes_horizontally();
+    uint32_t chg_verti = changes_vertically();
+
+    if (chg_horiz <= chg_verti) {
+        return DIRECTION_HORIZONTAL;
+    } else {
+        return DIRECTION_VERTICAL;
+    }
+
+    // Unreachable
+    return DIRECTION_HORIZONTAL;
+}
+
+/** Calculate how many times values change in the image in the horizontal
+ * direction. Used for determinig in which direction run-length encoding should
+ * be implemented.
+ * @returns The ammount of times pixel runs in the horizontal direction
+ * changed value.
+ */
+uint32_t Codec::changes_horizontally()
+{
+    uint32_t change_count = 0;
+    uint8_t previous = (*this->img)[0];
+
+    for (uint32_t i = 1; i < this->img->size(); i++) {
+        if (previous != (*this->img)[i]) {
+            change_count++;
+        }
+        previous = (*this->img)[i];
+    }
+
+    return change_count;
+}
+
+/** Calculate how many times values change in the image in the vertical
+ * direction. Used for determinig in which direction run-length encoding should
+ * be implemented.
+ * @returns The ammount of times pixel runs in the vertical direction
+ * changed value.
+ */
+uint32_t Codec::changes_vertically()
+{
+    uint32_t width, height;
+    this->img->dimensions(&width, &height);
+    const uint32_t stride = width;
+
+    uint32_t change_count = 0;
+    uint8_t previous = (*this->img)[0];
+
+    for (uint32_t i = 0; i < width; i++) {
+        for (uint32_t k = 0, j = i; k < height; k++, j += stride) {
+            // Used two variables in this loop so I don't have to do arithmetic.
+            if (previous != (*this->img)[j]) {
+                change_count++;
+            }
+            previous = (*this->img)[j];
+        }
+    }
+
+    return change_count;
+}
--- MIT1/KKO/proj/Codec.hpp
+++ MIT1/KKO/proj/Codec.hpp
@@ -1,13 +1,21 @@
 #ifndef CODEC_HPP
 #define CODEC_HPP
 
+#include <cstdint>
 #include <vector>
 #include "Image.hpp"
+
+#define DIRECTION_VERTICAL 1
+#define DIRECTION_HORIZONTAL 2
 
 class Codec
 {
 private:
     Image *img; //!< Pointer to an image to be encoded/decoded.
+
+    uint32_t changes_vertically();
+    uint32_t changes_horizontally();
+    uint8_t best_encoding_direction();
 public:
     Codec(Image *);
     ~Codec();
